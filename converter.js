04fde5ec1123b4b8dec79ed902923ef89050ed89
Rainer Stropek <rainer@timecockpit.com>
2017-09-27 14:35:04 +0200

    Added tips
    I added some tips as comments to your code. By following these tips, you could easily turn the tests green and earn an extra point for your grade. Hope that helps.
    
    Regards,
    Rainer Stropek.

--- converter.js
+++ converter.js
@@ -8,6 +8,9 @@ var lengths = ["m", "cm", "mm"];
 check();
 
 function failureHandler(){
+  // Change the message to "Invalid parameters" as this was the specified error message.
+  // The unit tests check for this message. If you change the message, many unit tests
+  // will become green.
   console.log("Something went wrong, try again!");
   process.exit();
 }
@@ -18,6 +21,9 @@ function failureHandler(){
    process.argv[5] sollte immer die zweite Einheit sein in die Umgewandelt wird.
 */
 function correctSyntax(){
+  // Remember the rule: Don't repeat yourself. In this case, you have four identical calls
+  // to `failureHandler`. Why not combining all `if` expressions in a single `if` statement
+  // and combining them using logical OR (`||`)?
   if(isNaN(process.argv[2])){
     failureHandler();
   }
@@ -33,6 +39,9 @@ function correctSyntax(){
 }
 
 function isIn(param){
+  // Note that JavaScript offers a `find` method for arrays
+  // (see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/find).
+  // you could simplify your code using this method.
   for(var j = 0; j<lengths.length; j++){
     if(param == lengths[j]){
       return true;
@@ -52,9 +61,13 @@ function isIn(param){
 
 function check(){
     correctSyntax();
+    // General comment: Remember "don't repeat yourself". Your `case` blocks look extremely similiar.
+    // You could try to create a solution where at least parts of the similar code pieces are only written once.
     switch(process.argv[3]){
       case "kg": 
                     switch(process.argv[5]){
+                      // You should add a whitespace (blank) between the number and the unit. That will turn
+                      // many tests green.
                       case "kg": console.log(process.argv[2]+process.argv[3]+" are "+process.argv[2]+process.argv[5]);break;
 
                       case "g": console.log(process.argv[2]+process.argv[3]+" are "+process.argv[2]*kiloToGramm+process.argv[5]); break;
